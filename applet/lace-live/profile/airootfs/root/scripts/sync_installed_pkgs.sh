198633d391377b695731d85f4fcce1b3a093a6a1
Rod Kay <rodakay5@gmail.com>
2022-08-30 00:31:37 +1000

    live.profile,scripts: add '--mkpath' to rsync.

--- applet/lace-live/profile/airootfs/root/scripts/sync_installed_pkgs.sh
+++ applet/lace-live/profile/airootfs/root/scripts/sync_installed_pkgs.sh
@@ -8,6 +8,7 @@ set -E
 mount /dev/sda2 /mnt
 
 rsync -av                          \
+      --mkpath                     \
       /mnt/var/cache/pacman/pkg/   \
       rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live/profile/airootfs/root/custom/var/cache/pacman/pkg
       
