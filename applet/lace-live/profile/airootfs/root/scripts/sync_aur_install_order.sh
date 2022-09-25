a0db8c916b22b7234829a9db129a320dbdba5ea1
Rod Kay <rodakay5@gmail.com>
2022-09-25 22:47:25 +1000

    sync_aur_install_order.sh: Fix 'rsync' target folder.

--- applet/lace-live/profile/airootfs/root/scripts/sync_aur_install_order.sh
+++ applet/lace-live/profile/airootfs/root/scripts/sync_aur_install_order.sh
@@ -3,6 +3,6 @@
 set -e
 
 rsync -av /root/aur_install_order \
-          rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live
+          rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live/profile/airootfs/root/
 
 echo Done.
