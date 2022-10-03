e3e798e5ba2dc731908121758a223c7068056659
Rod Kay <rodakay5@gmail.com>
2022-10-04 00:28:05 +1100

    lace-live.scripts: Rid '--delete' from 'rsync's.

--- applet/lace-live/profile/airootfs/root/scripts/sync_installed_pkgs.sh
+++ applet/lace-live/profile/airootfs/root/scripts/sync_installed_pkgs.sh
@@ -9,7 +9,6 @@ mount /dev/vda2 /mnt
 
 rsync -av                          \
       --mkpath                     \
-      --delete                     \
       /mnt/var/cache/pacman/pkg/   \
       rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live/profile/airootfs/root/custom/var/cache/pacman/pkg
       
