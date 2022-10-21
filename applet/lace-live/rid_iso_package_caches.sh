628c4615ad29c55b405ca6d78201f14bcaba8a56
Rod Kay <rodakay5@gmail.com>
2022-10-21 17:02:00 +1100

    Cosmetics.

--- applet/lace-live/rid_iso_package_caches.sh
+++ applet/lace-live/rid_iso_package_caches.sh
@@ -3,6 +3,6 @@
 set -e
 
 sudo rm -fr profile/airootfs/root/aur/*
-sudo rm -fr profile/airootfs/root/custom//var/cache/pacman/pkg/*
+sudo rm -fr profile/airootfs/root/custom/var/cache/pacman/pkg/*
 
 echo Done.
