7cb2375708bd8d8e5221c07d0aba98dd399bf470
Rod Kay <rodakay5@gmail.com>
2022-10-24 17:48:50 +1100

    lace-live.profile.root.scripts.sync_essential_pkgs.sh: Sync to 'pacstrap_packages'.

--- applet/lace-live/profile/airootfs/root/scripts/sync_essential_pkgs.sh
+++ applet/lace-live/profile/airootfs/root/scripts/sync_essential_pkgs.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-set -E
-
-#export IP=192.168.165.253
-#export IP=192.168.33.67
+set -e
 
 rsync -av                      \
       /var/cache/pacman/pkg/   \
-      rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live/profile/airootfs/root/custom/var/cache/pacman/pkg
+      rod@$IP:/eden/forge/applet/os/laceOS/applet/lace-live/profile/airootfs/root/pacstrap_packages
       
 echo Done.
