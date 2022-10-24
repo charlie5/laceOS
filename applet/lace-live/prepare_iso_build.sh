a8acd52717cb9f4adc77c7d326773d2f3ac8c4a2
Rod Kay <rodakay5@gmail.com>
2022-10-24 17:39:33 +1100

    lace-live: Add progress reports to 'prepare_iso_build.sh'.

--- applet/lace-live/prepare_iso_build.sh
+++ applet/lace-live/prepare_iso_build.sh
@@ -3,17 +3,41 @@
 set -e
 
 export LACE_LIVE='/eden/forge/applet/os/laceOS/applet/lace-live'
+cd    $LACE_LIVE
 
-cd $LACE_LIVE
 
+echo
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Building builder_packages.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo
+
+sleep 2
 pikaur -Syu
 rm -fr ~/.cache/pikaur/*
 pikaur -S --noconfirm pikaur
 pikaur -S --noconfirm xmlada libgpr gprbuild
 rsync -av ~/.cache/pikaur/pkg/ profile/airootfs/root/builder_packages
 
+
+echo
+echo ~~~~~~~~~~~~~~~~~~~~~~~
+echo Rid ISO package caches.
+echo ~~~~~~~~~~~~~~~~~~~~~~~
+echo
+
+sleep 2
 ./rid_iso_package_caches.sh
 ./rid_iso.sh
+
+
+echo
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Build and export the ISO.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo
+
+sleep 2
 ./build_iso.sh
 ./export_iso.sh
 
