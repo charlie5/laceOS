e84afdbe3ab31929583f4a86cb9da03a5e411a81
Rod Kay <rodakay5@gmail.com>
2022-10-24 17:43:13 +1100

    lace-live.rid_iso_package_caches.sh: Rid pacstrap_packages.

--- applet/lace-live/rid_iso_package_caches.sh
+++ applet/lace-live/rid_iso_package_caches.sh
@@ -4,5 +4,6 @@ set -e
 
 sudo rm -fr profile/airootfs/root/aur/*
 sudo rm -fr profile/airootfs/root/custom/var/cache/pacman/pkg/*
+sudo rm -fr profile/airootfs/root/pacstrap_packages/*
 
 echo Done.
