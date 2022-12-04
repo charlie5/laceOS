b99f92436fa74e50441e33d0fffed4169d336231
Rod Kay <rodakay5@gmail.com>
2022-12-04 18:45:08 +1100

    lace-live.build_iso.sh: Add identity tags in call to 'mkarchiso'.

--- applet/lace-live/build_iso.sh
+++ applet/lace-live/build_iso.sh
@@ -5,6 +5,11 @@ set -e
 cp ../installer/install_laceOS  \
    profile/airootfs/usr/local/bin
    
-sudo mkarchiso -v -w /tmp/archiso-tmp profile
+sudo mkarchiso -v                        \
+               -A "laceOS Installer"     \
+               -L "laceOS_ISO"           \
+               -P "Rod Kay"              \
+               -w /tmp/archiso-tmp       \
+               profile
 
 echo Done.
