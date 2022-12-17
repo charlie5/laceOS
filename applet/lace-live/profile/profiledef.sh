5f4b5d28f5e5b4c24c7e7b4cbb01dce720cf799d
Rod Kay <rodakay5@gmail.com>
2022-12-17 15:50:49 +1100

    lace-live: Increment release versions.

--- applet/lace-live/profile/profiledef.sh
+++ applet/lace-live/profile/profiledef.sh
@@ -7,7 +7,7 @@ iso_label="laceOS_$(date +%Y%m)"
 iso_publisher="Rod Kay <http://www.orthanc.site:8080>"
 iso_application="laceOS Linux Install/Rescue CD"
 #iso_version="$(date +%Y.%m.%d)"
-iso_version="0.3"
+iso_version="0.4"
 
 install_dir="laceos"
 
